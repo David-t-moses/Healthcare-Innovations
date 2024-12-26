3b3e8521139ea3376f94b0e70724d70e69f491da
David Iyerverh T. <kukudavid012@gmail.com>
2024-12-26 16:40:21 +0100

    update config

--- next.config.ts
+++ next.config.ts
@@ -7,19 +7,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  webpack: (config) => {
-    config.ignoreWarnings = [/^(?!CriticalDependenciesWarning$)/];
-    config.infrastructureLogging = {
-      level: "error",
-    };
-    return config;
-  },
-  onDemandEntries: {
-    maxInactiveAge: 25 * 1000,
-    pagesBufferLength: 2,
-  },
-  poweredByHeader: false,
-  productionBrowserSourceMaps: false,
 };
 
 export default nextConfig;
