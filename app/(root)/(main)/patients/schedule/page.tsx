0f62d21c11eb6186fb98d4f87b79bf378a465109
David Iyerverh T. <kukudavid012@gmail.com>
2025-05-24 02:51:05 +0100

    update

--- app/(root)/(main)/patients/schedule/page.tsx
+++ app/(root)/(main)/patients/schedule/page.tsx
@@ -1,6 +1,8 @@
 import AppointmentScheduler from "@/components/AppointmentScheduler";
 import { getCurrentUser } from "@/lib/auth";
 
+export const dynamic = "force-dynamic";
+
 export default async function SchedulePage({
   searchParams,
 }: {
